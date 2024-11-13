28401630b4d7274899716301e0e1aa5f32402f3c
amanda-bann <amanda_klap@protonmail.com>
2024-11-13 14:55:53 -0700

    Create a method for playtime

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ const createPet = function(name, species) {
         sleep: function() {
             console.log(`${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
+        },
+        play: function () {
+            if (this.isTired === 10) {
+                console.log(`${this.name} is too tired to play.`);
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
         }
     };
     return pet;
