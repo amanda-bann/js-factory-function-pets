f6c3f0cb3348004ab035e7723da8ea1bcf1f9c93
amanda-bann <amanda_klap@protonmail.com>
2024-11-13 14:43:22 -0700

    Create a factory function & add properties

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function(name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5 //scale of 1-5
+    };
+    return pet;
+};
