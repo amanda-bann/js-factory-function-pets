99d8ae07b425bff94c36bafdc7efa789155d05c2
amanda-bann <amanda_klap@protonmail.com>
2024-11-13 14:49:31 -0700

    Create a method for sleep

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ const createPet = function(name, species) {
     const pet = {
         name: name,
         species: species,
-        isTired: 5 //scale of 1-5
+        isTired: 5, //scale of 1-10 where 1 is rested and 10 is shattered
+        sleep: function() {
+            console.log(`${this.name} needs a nap. Zzz...`);
+            this.isTired = 1;
+        }
     };
     return pet;
 };
